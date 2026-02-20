d3b4d54f5ef26a508acd82618e440720d3333907
svdgamerz <jayeshwalavalkar12@gmail.com>
2026-02-20 11:02:14 +0530

    faahhhhhhh

---
+++ codes/python5.py
@@ -0,0 +1,13 @@
+class bankaccount:
+    bankname ="national bank"
+totalacc = 0
+totalbalance = 0
+def _init_(self,accountholdername):
+    self.balance = self.balance 
+    def deposit(self,account):
+        pass
+    def withdraw(self,account):
+        pass
+    def displayaccountholder(self,account):
+        pass
+    
